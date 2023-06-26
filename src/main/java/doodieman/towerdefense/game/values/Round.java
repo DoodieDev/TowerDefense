9ec7d38becc5044a6e2363a98659fd23681ffcad
Afro <86922948+AfroSnail@users.noreply.github.com>
2023-06-26 15:18:29 +0200

    Update Round.java

--- src/main/java/doodieman/towerdefense/game/values/Round.java
+++ src/main/java/doodieman/towerdefense/game/values/Round.java
@@ -55,20 +55,147 @@ public enum Round {
     )),
 
     ROUND_10(10, 25L, Arrays.asList(
-        new MobTypeStack(1,MobType.ZOMBIE4)
+        new MobTypeStack(3,MobType.ZOMBIE4)
     )),
 
-    ROUND_11(11, 20L, Arrays.asList(
+
+    
+    // 10 ROUNDS COMPLETED
+
+    
+
+    ROUND_11(11, 15L, Arrays.asList(
         new MobTypeStack(25,MobType.ZOMBIE3),
-        new MobTypeStack(10,MobType.SKELETON3)
+        new MobTypeStack(10,MobType.SKELETON3),
+        new MobTypeStack(5,MobType.PIGMAN1)
     )),
 
     ROUND_12(12, 5L, Arrays.asList(
         new MobTypeStack(30,MobType.ZOMBIE1),
         new MobTypeStack(15,MobType.ZOMBIE2),
         new MobTypeStack(10,MobType.SKELETON3)
-    ));
+    )),
 
+    ROUND_13(13, 10L, Arrays.asList(
+        new MobTypeStack(50,MobType.SKELETON1),
+        new MobTypeStack(15,MobType.SKELETON2)
+    )),
+
+    ROUND_14(14, 3L, Arrays.asList(
+        new MobTypeStack(25,MobType.ZOMBIE1),
+        new MobTypeStack(10,MobType.SKELETON3),
+        new MobTypeStack(25,MobType.ZOMBIE1),
+        new MobTypeStack(10,MobType.SKELETON3)
+    )),
+
+    ROUND_15(15, 7.5L, Arrays.asList(
+        new MobTypeStack(20,MobType.ZOMBIE1),
+        new MobTypeStack(15,MobType.ZOMBIE2),
+        new MobTypeStack(10,MobType.ZOMBIE3),
+        new MobTypeStack(20,MobType.SKELETON1),
+        new MobTypeStack(15,MobType.SKELETON3),
+        new MobTypeStack(10,MobType.SKELETON3),
+        new MobTypeStack(5,MobType.PIGMAN1)
+    )),
+
+    ROUND_16(16, 10L, Arrays.asList(
+        new MobTypeStack(40,MobType.ZOMBIE3),
+        new MobTypeStack(10,MobType.SKELETON3),
+        new MobTypeStack(10,MobType.SKELETON2),
+        new MobTypeStack(10,MobType.PIGMAN2)
+    )),
+
+    ROUND_17(17, 3L, Arrays.asList(
+        new MobTypeStack(50,MobType.SKELETON2)
+    )),
+
+    ROUND_18(18, 5L, Arrays.asList(
+        new MobTypeStack(80,MobType.ZOMBIE3),
+        new MobTypeStack(20,MobType.SKELETON4)
+    )),
+
+    ROUND_19(19, 2.5L, Arrays.asList(
+        new MobTypeStack(50,MobType.SKELETON1),
+        new MobTypeStack(20,MobType.SKELETON3),
+        new MobTypeStack(10,MobType.SKELETON4)
+    )),
+
+    ROUND_20(20, 25L, Arrays.asList(
+        new MobTypeStack(5,MobType.SKELETON5)
+    )),
+
+
+    
+    // 20 ROUNDS COMPLETED (PROBABLY START GIVING EM IRON/DIA GEAR)
+
+    
+
+    ROUND_21(21, 7.5L, Arrays.asList(
+        new MobTypeStack(40,MobType.PIGMAN1),
+        new MobTypeStack(15,MobType.PIGMAN2),
+    )),
+
+    ROUND_22(22, 5L, Arrays.asList(
+        new MobTypeStack(20,MobType.SKELETON4),
+        new MobTypeStack(15,MobType.ZOMBIE3)
+    )),
+
+    ROUND_23(23, 10L, Arrays.asList(
+        new MobTypeStack(10,MobType.PIGMAN3)
+    )),
+
+    ROUND_24(24, 1.5L, Arrays.asList(
+        new MobTypeStack(50,MobType.SKELETON2),
+        new MobTypeStack(1,MobType.PIGMAN4),
+        new MobTypeStack(25,MobType.ZOMBIE3)
+    )),
+
+    ROUND_25(25, 3L, Arrays.asList(
+        new MobTypeStack(25,MobType.ZOMBIE3),
+        new MobTypeStack(15,MobType.SKELETON3),
+        new MobTypeStack(30,MobType.SKELETON4)
+    )),
+
+    ROUND_26(26, 5L, Arrays.asList(
+        new MobTypeStack(30,MobType.PIGMAN1),
+        new MobTypeStack(15,MobType.PIGMAN2)
+    )),
+
+    ROUND_27(27, 2L, Arrays.asList(
+        new MobTypeStack(100,MobType.ZOMBIE2),
+        new MobTypeStack(60,MobType.SKELETON1),
+        new MobTypeStack(45,MobType.ZOMBIE3),
+        new MobTypeStack(45,MobType.SKELETON3)
+    )),
+
+    ROUND_28(28, 10L, Arrays.asList(
+        new MobTypeStack(10,MobType.PIGMAN4),
+        new MobTypeStack(10,MobType.SKELETON4),
+        new MobTypeStack(10,MobType.PIGMAN4),
+        new MobTypeStack(10,MobType.SKELETON4),
+        new MobTypeStack(10,MobType.PIGMAN4),
+        new MobTypeStack(10,MobType.SKELETON4)
+    )),
+
+    ROUND_29(29, 5L, Arrays.asList(
+        new MobTypeStack(30,MobType.ZOMBIE3),
+        new MobTypeStack(30,MobType.SKELETON4)
+    )),
+
+    ROUND_30(30, 25L, Arrays.asList(
+        new MobTypeStack(10,MobType.PIGMAN5)
+    )),
+
+
+
+    // 30 ROUNDS COMPLETED
+
+
+    
+    ROUND_31(31, 1L, Arrays.asList(
+        new MobTypeStack(100,MobType.PIGMAN3),
+        new MobTypeStack(200,MobType.SKELETON2)
+    ));
 
 
     @Getter
